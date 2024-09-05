746cff0be0a761df8e99d4d0b680181818b55917
Antranig Basman <amb26ohloh@ponder.org.uk>
2024-09-05 12:32:06 +0100

    First working version with plates and inset layout

--- post_build.R
+++ post_build.R
@@ -1,3 +1,9 @@
-R.utils::copyDirectory("css", "docs/css")
-R.utils::copyDirectory("img", "docs/img")
-R.utils::copyDirectory("fonts", "docs/fonts")
+copy_dir <- function (from, to) {
+  unlink(to, recursive = TRUE)
+  R.utils::copyDirectory(from, to)
+}
+
+copy_dir("css", "docs/css")
+copy_dir("img", "docs/img")
+copy_dir("fonts", "docs/fonts")
+copy_dir("js", "docs/js")
