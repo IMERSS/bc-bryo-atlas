0750f2ac1007d66896d8ce3aef3f1140afc6f2f9
Antranig Basman <amb26ohloh@ponder.org.uk>
2024-11-15 15:37:39 +0000

    First version with maps, added frequency field

--- generate_pages.R
+++ generate_pages.R
@@ -4,23 +4,41 @@ library(readr)
 library(stringr)
 library(dplyr)
 library(tidyr)
+library(googledrive)
 
-template <- readr::read_file("templates/taxon.qmdt")
-gsub("\r\n", "\n", template)
-
-taxa <- read_sheet("1MG7C7GX1Tl2RO_vHuMwUo8quhzYZd_mElWRnPuNbpj8", sheet = "friendly") %>%
-    mutate_all(~replace_na(as.character(.), ""))
-names(taxa)[names(taxa) == "quick&Dirty"] <- "quickNDirty"
-
-make_page <- function (row) {
-  templated <- whisker.render(template, data=row)
-  filename <- str_glue("taxa/{row$taxon}.qmd")
-  file <- file(filename, "wb")
-  write(templated, file)
-  close(file)
-  writeLines(str_glue("Written {nchar(templated)} bytes to file {filename}"))
+downloadGdrive <- function (id, file_path) {
+  if (!file.exists(file_path)) {
+    drive_download(as_id(id), path = file_path, overwrite = FALSE)
+  }
 }
 
-for (i in 1:nrow(taxa)) {
-   make_page(taxa[i,])
+# Found existence of these variables at https://quarto.org/docs/projects/scripts.html#pre-and-post-render
+render_all <- nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))
+
+if (render_all) {
+  
+  # Fetch from BC Bryophyte Guide/Catalogues/GBIF-0002297-240626123714530.csv
+  downloadGdrive("1UlByeYlHRyKBl2xnmbF_tVG4jl_xcO3R", "tabular_data/GBIF-0002297-240626123714530.csv")
+  
+  template <- readr::read_file("templates/taxon.qmdt")
+  gsub("\r\n", "\n", template)
+  
+  taxa <- read_sheet("1MG7C7GX1Tl2RO_vHuMwUo8quhzYZd_mElWRnPuNbpj8", sheet = "friendly") %>%
+    mutate_all(~replace_na(as.character(.), ""))
+  names(taxa)[names(taxa) == "quick&Dirty"] <- "quickNDirty"
+  
+  make_page <- function (row) {
+    templated <- whisker.render(template, data=row)
+    filename <- str_glue("taxa/{row$taxon}.qmd")
+    file <- file(filename, "wb")
+    write(templated, file)
+    close(file)
+    writeLines(str_glue("Written {nchar(templated)} bytes to file {filename}"))
+  }
+  
+  for (i in seq_len(nrow(taxa))) {
+    make_page(taxa[i,])
+  }
+} else {
+  cat("Not rendering all files, skipping site generation")
 }
--- page_functions.R
+++ page_functions.R
@@ -3,46 +3,54 @@
 library(plotly)
 suppressPackageStartupMessages(library(tidyverse))
 
-
 # make species map
-gbif <- read_delim("../../files/0037810-231120084113126.csv", delim='\t') %>%
-  mutate(date = ymd(date(eventDate)), Year = year(date),
+rawGbif <- read_delim("tabular_data/GBIF-0002297-240626123714530.csv", delim='\t')
+gbif <- rawGbif %>%
+  mutate(date = parse_date_time(eventDate, "Ymd HMS", truncated = 3), Year = year(date),
          Month = lubridate::month(date, label=T),
          lat=decimalLatitude, lon=decimalLongitude) %>%
-  drop_na(date, lat, lon, genus, occurrenceID) %>%
-  separate_wider_delim(species, ' ', names=c('Genus', 'Species'), too_many = 'merge') %>%
-  mutate(Species=ifelse(Species=='nr', NA, Species))
+  drop_na(date, lat, lon, genus, occurrenceID)
 
-
-make_species_map <- function(species){
-  if (!(species %in% gbif$Species)){
+make_species_map <- function (thisSpecies) {
+  if (!(thisSpecies %in% gbif$species)) {
     return("no GBIF data to display")
   }
+  thisRecords <- gbif %>% filter(species == thisSpecies)
   
-  gbif %>% 
-    filter(Species == species) %>%
-    plot_ly() %>%
+  lat_range <- range(thisRecords$lat)
+  lon_range <- range(thisRecords$lon)
+  
+  # Center of the bounding box
+  center <- list(lon = mean(lon_range), lat = mean(lat_range))
+  
+  # A rough estimate for zoom level based on bounds
+  zoom <- min(20, max(1, 8 - log(diff(lat_range) + diff(lon_range)))) - 1
+  
+  thisRecords %>% plot_ly() %>%
     add_trace(
       lat = ~lat, lon =~lon,
       mode = 'markers', type = 'scattermapbox',
       color = ~Year,
       text = ~paste(
         basisOfRecord,
-        '\nSpecies:', Genus, Species, 
-        '\nObserved:', stamp("March 1, 1999")(date)
+        '\nSpecies:', species, 
+        '\nObserved:', stamp("March 1, 1999", quiet = TRUE)(date)
         ),
-      size=10,
-      name= ~Year,
-      marker= list(opacity=0.6),
-      showlegend=F
-    ) %>%
-    colorbar(tick0=0,
-             dtick=max(3, ceiling(length(unique((filter(gbif, Species %in% species)$Year)))/3))) %>%
+      size = 10,
+      showlegend = FALSE, # doesn't work
+      name = ~Year,
+      marker = list(opacity=0.6)
+    ) %>% colorbar(tick0=0,
+       dtick=max(5, ceiling(length(unique((thisRecords$Year)))/3)),
+       thickness=10) %>%
     layout(mapbox = list(
-      style = 'carto-positron', show_legend = T,
-      #zoom=7, center = list(lon = -123, lat = 49)),
-      zoom=3.8, center = list(lon = -127, lat = 50)),
-      title = 'GBIF observations in the Pacific Maritime region') %>%
-    return()
+      style = 'carto-positron',
+      zoom = zoom, center = center
+    ),
+    showlegend = FALSE, # These directives don't work, can't suppress legend
+    legend = list(x = 0.9, y = 0.9),
+    autosize = TRUE,
+    margin = list(t = 0, r = 0, b = 0, l = 0)
+    )
 }
 
