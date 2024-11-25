9cd223ef4176fee72d3ffbc4be254a730346dfc4
Antranig Basman <amb26ohloh@ponder.org.uk>
2024-11-25 16:34:39 +0000

    Regenerated site, streamlined CSV access

--- generate_pages.R
+++ generate_pages.R
@@ -1,14 +1,10 @@
 library(whisker)
-library(googlesheets4)
-library(readr)
-library(stringr)
-library(dplyr)
-library(tidyr)
+library(tidyverse)
 library(googledrive)
 
-downloadGdrive <- function (id, file_path) {
-  if (!file.exists(file_path)) {
-    drive_download(as_id(id), path = file_path, overwrite = FALSE)
+downloadGdrive <- function (id, file_path, overwrite = FALSE) {
+  if (overwrite || !file.exists(file_path)) {
+    drive_download(as_id(id), path = file_path, overwrite = TRUE)
   }
 }
 
@@ -23,9 +19,13 @@ if (render_all) {
   template <- readr::read_file("templates/taxon.qmdt")
   gsub("\r\n", "\n", template)
   
-  taxa <- read_sheet("1MG7C7GX1Tl2RO_vHuMwUo8quhzYZd_mElWRnPuNbpj8", sheet = "friendly") %>%
+  # Fetch from BC Bryophyte Guide/BC_Bryo_Guide
+  downloadGdrive("1MG7C7GX1Tl2RO_vHuMwUo8quhzYZd_mElWRnPuNbpj8", "tabular_data/BC_Bryo_Guide.csv", TRUE)
+  taxa <- read.csv("tabular_data/BC_Bryo_Guide.csv") %>%
     mutate_all(~replace_na(as.character(.), ""))
-  names(taxa)[names(taxa) == "quick&Dirty"] <- "quickNDirty"
+  
+  # Note that whisker templating does not permit period characters in key names
+  names(taxa)[names(taxa) == "quick.Dirty"] <- "quickNDirty"
   
   make_page <- function (row) {
     templated <- whisker.render(template, data=row)
